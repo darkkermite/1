944536d7fa46b6944747a9e61f6e322a13683019
darkkermite <163955268+darkkermite@users.noreply.github.com>
2024-03-23 23:46:25 -0700

    javascript.js

--- Untitled-3.js
+++ Untitled-3.js
@@ -1,3 +1,14 @@
 document.getElementById("clickMeBtn").addEventListener("click", function() {
-    alert("You clicked the button!");
+    // Function to generate a random color
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    // Change the background color of the body to a random color
+    document.body.style.backgroundColor = getRandomColor();
 });
