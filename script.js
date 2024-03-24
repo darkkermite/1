97969dd361ccf7f69c5ad81865a7795452e26453
darkkermite <163955268+darkkermite@users.noreply.github.com>
2024-03-24 00:20:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-document.getElementById("clickMeBtn").addEventListener("click", function() {
-    // Function to generate a random color
+document.getElementById("changeColorBtn").addEventListener("click", function() {
     function getRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
             color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
     }
-
-    // Change the background color of the body to a random color
+    
     document.body.style.backgroundColor = getRandomColor();
 });
